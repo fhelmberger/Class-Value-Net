c3284554f90655f20a1a472782a00b8a1c1e6c4e
Marcel Gruenauer <hanekomu@gmail.com>
2008-07-25 13:57:25 +0000

    continued to update distribution to standard style
    git-svn-id: http://svn.coderepos.org/share/lang/perl/Class-Value-Net/trunk@16451 d0d07461-0603-4401-acd4-de1884942a52

--- lib/Class/Value/Net/Exception/InvalidIPAddress.pm
+++ lib/Class/Value/Net/Exception/InvalidIPAddress.pm
@@ -26,15 +26,15 @@ use constant PROPERTIES => (qw/ipaddr/);
 
 __END__
 
-{% USE p = PodGenerated %}
+
 
 =head1 NAME
 
-{% p.package %} - network-related value objects
+Class::Value::Net::Exception::InvalidIPAddress - network-related value objects
 
 =head1 SYNOPSIS
 
-    {% p.package %}->new;
+    Class::Value::Net::Exception::InvalidIPAddress->new;
 
 =head1 DESCRIPTION
 
@@ -45,13 +45,106 @@ next release will have more documentation.
 
 =over 4
 
-{% p.write_methods %}
+=item clear_ipaddr
+
+    $obj->clear_ipaddr;
+
+Clears the value.
+
+=item ipaddr
+
+    my $value = $obj->ipaddr;
+    $obj->ipaddr($value);
+
+A basic getter/setter method. If called without an argument, it returns the
+value. If called with a single argument, it sets the value.
+
+=item ipaddr_clear
+
+    $obj->ipaddr_clear;
+
+Clears the value.
 
 =back
 
-{% p.write_inheritance %}
+Class::Value::Net::Exception::InvalidIPAddress inherits from
+L<Class::Value::Net::Exception>.
 
-{% PROCESS standard_pod %}
+The superclass L<Error::Hierarchy> defines these methods and functions:
+
+    acknowledged(), acknowledged_clear(), acknowledged_set(),
+    clear_acknowledged(), clear_is_optional(), comparable(), error_depth(),
+    get_properties(), init(), is_optional(), is_optional_clear(),
+    is_optional_set(), properties_as_hash(), set_acknowledged(),
+    set_is_optional(), stringify(), transmute()
+
+The superclass L<Error::Hierarchy::Base> defines these methods and
+functions:
+
+    new(), dump_as_yaml(), dump_raw()
+
+The superclass L<Error> defines these methods and functions:
+
+    _throw_Error_Simple(), associate(), catch(), file(), flush(), import(),
+    object(), prior(), record(), text(), throw(), value(), with()
+
+The superclass L<Data::Inherited> defines these methods and functions:
+
+    every_hash(), every_list(), flush_every_cache_by_key()
+
+The superclass L<Class::Accessor::Complex> defines these methods and
+functions:
+
+    mk_abstract_accessors(), mk_array_accessors(), mk_boolean_accessors(),
+    mk_class_array_accessors(), mk_class_hash_accessors(),
+    mk_class_scalar_accessors(), mk_concat_accessors(),
+    mk_forward_accessors(), mk_hash_accessors(), mk_integer_accessors(),
+    mk_new(), mk_object_accessors(), mk_scalar_accessors(),
+    mk_set_accessors(), mk_singleton()
+
+The superclass L<Class::Accessor> defines these methods and functions:
+
+    _carp(), _croak(), _mk_accessors(), accessor_name_for(),
+    best_practice_accessor_name_for(), best_practice_mutator_name_for(),
+    follow_best_practice(), get(), make_accessor(), make_ro_accessor(),
+    make_wo_accessor(), mk_accessors(), mk_ro_accessors(),
+    mk_wo_accessors(), mutator_name_for(), set()
+
+The superclass L<Class::Accessor::Installer> defines these methods and
+functions:
+
+    install_accessor()
+
+=head1 BUGS AND LIMITATIONS
+
+No bugs have been reported.
+
+Please report any bugs or feature requests through the web interface at
+L<http://rt.cpan.org>.
+
+=head1 INSTALLATION
+
+See perlmodinstall for information and options on installing Perl modules.
+
+=head1 AVAILABILITY
+
+The latest version of this module is available from the Comprehensive Perl
+Archive Network (CPAN). Visit <http://www.perl.com/CPAN/> to find a CPAN
+site near you. Or see <http://www.perl.com/CPAN/authors/id/M/MA/MARCEL/>.
+
+=head1 AUTHORS
+
+Heinz Ekker C<< <ek@univie.ac.at> >>
+
+Marcel GrE<uuml>nauer, C<< <marcel@cpan.org> >>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright 2004-2008 by the authors.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
 
 =cut
 
--- lib/Class/Value/Net/NAPTR/Preference_TEST.pm
+++ lib/Class/Value/Net/NAPTR/Preference_TEST.pm
@@ -22,15 +22,15 @@ sub expected_malformed_exception {
 
 __END__
 
-{% USE p = PodGenerated %}
+
 
 =head1 NAME
 
-{% p.package %} - network-related value objects
+Class::Value::Net::NAPTR::Preference_TEST - network-related value objects
 
 =head1 SYNOPSIS
 
-    {% p.package %}->new;
+    Class::Value::Net::NAPTR::Preference_TEST->new;
 
 =head1 DESCRIPTION
 
@@ -41,13 +41,81 @@ next release will have more documentation.
 
 =over 4
 
-{% p.write_methods %}
+
 
 =back
 
-{% p.write_inheritance %}
+Class::Value::Net::NAPTR::Preference_TEST inherits from
+L<Class::Value::Net::NAPTR::UInt16_TEST>.
 
-{% PROCESS standard_pod %}
+The superclass L<Class::Value::Net::NAPTR::UInt16_TEST> defines these
+methods and functions:
+
+    run()
+
+The superclass L<Test::CompanionClasses::Base> defines these methods and
+functions:
+
+    new(), clear_package(), make_real_object(), package(), package_clear(),
+    planned_test_count()
+
+The superclass L<Class::Accessor::Complex> defines these methods and
+functions:
+
+    mk_abstract_accessors(), mk_array_accessors(), mk_boolean_accessors(),
+    mk_class_array_accessors(), mk_class_hash_accessors(),
+    mk_class_scalar_accessors(), mk_concat_accessors(),
+    mk_forward_accessors(), mk_hash_accessors(), mk_integer_accessors(),
+    mk_new(), mk_object_accessors(), mk_scalar_accessors(),
+    mk_set_accessors(), mk_singleton()
+
+The superclass L<Class::Accessor> defines these methods and functions:
+
+    _carp(), _croak(), _mk_accessors(), accessor_name_for(),
+    best_practice_accessor_name_for(), best_practice_mutator_name_for(),
+    follow_best_practice(), get(), make_accessor(), make_ro_accessor(),
+    make_wo_accessor(), mk_accessors(), mk_ro_accessors(),
+    mk_wo_accessors(), mutator_name_for(), set()
+
+The superclass L<Class::Accessor::Installer> defines these methods and
+functions:
+
+    install_accessor()
+
+The superclass L<Data::Inherited> defines these methods and functions:
+
+    every_hash(), every_list(), flush_every_cache_by_key()
+
+=head1 BUGS AND LIMITATIONS
+
+No bugs have been reported.
+
+Please report any bugs or feature requests through the web interface at
+L<http://rt.cpan.org>.
+
+=head1 INSTALLATION
+
+See perlmodinstall for information and options on installing Perl modules.
+
+=head1 AVAILABILITY
+
+The latest version of this module is available from the Comprehensive Perl
+Archive Network (CPAN). Visit <http://www.perl.com/CPAN/> to find a CPAN
+site near you. Or see <http://www.perl.com/CPAN/authors/id/M/MA/MARCEL/>.
+
+=head1 AUTHORS
+
+Heinz Ekker C<< <ek@univie.ac.at> >>
+
+Marcel GrE<uuml>nauer, C<< <marcel@cpan.org> >>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright 2004-2008 by the authors.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
 
 =cut
 
