47cbee0e76c841b02b03856372cdd2125ea087b7
Marcel Gruenauer <hanekomu@gmail.com>
2010-06-24 12:53:24 +0200

    use Data::Semantic::Net::IPAddress::TestData::IPv4 1.101750

--- lib/Class/Value/Net/IPAddress/IPv4_TEST.pm
+++ lib/Class/Value/Net/IPAddress/IPv4_TEST.pm
@@ -4,7 +4,7 @@ use warnings;
 
 package Class::Value::Net::IPAddress::IPv4_TEST;
 
-use Data::Semantic::Net::IPAddress::TestData::IPv4 1.101730;
+use Data::Semantic::Net::IPAddress::TestData::IPv4 1.101750;
 # ABSTRACT: Network-related value objects
 use parent qw(
   Class::Value::Test
